7f8c4e3c97931fa42740f09e64006726d697a43a
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-07 10:47:44 -0300

    Add Synchronous Validation to a redux-form using a field-level Validation

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,21 +1,32 @@
-export const validate = (values) => {
-  const errors = {};
+// Opcion 1: Synchronous Validation Function
+// export const validate = (values) => {
+//   const errors = {};
 
-  if (!values.firstname) {
-    errors.firstname = "First name is required";
-  }
+//   if (!values.firstname) {
+//     errors.firstname = "First name is required";
+//   }
 
-  if (!values.surname) {
-    errors.surname = "Surname is required";
-  }
+//   if (!values.surname) {
+//     errors.surname = "Surname is required";
+//   }
 
-  if (!values.username) {
-    errors.username = "Username is required";
-  } else if (values.username.length < 4) {
-    errors.username = "Username must be at least 4 characters long";
-  } else if (values.username.length > 10) {
-    errors.username = "Username is too long";
-  }
+//   if (!values.username) {
+//     errors.username = "Username is required";
+//   } else if (values.username.length < 4) {
+//     errors.username = "Username must be at least 4 characters long";
+//   } else if (values.username.length > 10) {
+//     errors.username = "Username is too long";
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
+
+
+//Opcion 2 Synchronous Field level validation
+export const required = (value) => (value ? undefined : "Value is required");
+
+export const minLength = (value) =>
+  value.length < 4 ? "Value must be at least 4 characters" : undefined;
+
+export const maxLength = (value) =>
+  value.length > 10 ? "Value is too long" : undefined;
