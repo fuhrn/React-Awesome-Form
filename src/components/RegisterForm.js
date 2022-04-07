0f31bc6b484c27fba960e82c0969243240752643
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-04-07 09:04:50 -0300

    Use a Custom Input Component as a redux-form Field

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,32 +1,21 @@
 import React, {Component} from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { customInput, customSelect } from './fields';
 
 class RegisterForm extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">Name</label>
-          <Field name="name" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="">Preferred Formatting</label>
-          <Field name="preference" component="select" >
-            <option />
-            <option value="tabs">Tabs</option>
-            <option value="spaces">Spaces</option>
-          </Field>
-        </div>
-        <div>
-          <label htmlFor="">Sign up to newsletter?</label>
-          <Field
-            name='newsletter'
-            component='input'
-            type='checkbox'
-          />
-        </div>
-        <button type='submit'>Submit</button>
+        <Field name="name" component={customInput} type="text" label="Name" />
+        <Field
+          name="preference"
+          component={customSelect}
+          label="Preferred Formatting"
+        />
+        <Field name="newsletter" component={customInput} type="checkbox" label="Sign up to Newsletter" />
+
+        <button type="submit">Submit</button>
       </form>
     );
   }
